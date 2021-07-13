758a7c2c3a7527d63fd2af2d2ab15bd4064884c5
Marek Dudek <marek.dudek@gmail.com>
2021-07-13 19:46:49 +0200

    message produced

---
+++ src/main/java/md/KafkaProducerApplication.java
@@ -0,0 +1,29 @@
+package md;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class KafkaProducerApplication
+{
+    public static void main(String[] args) throws ExecutionException, InterruptedException
+    {
+        final Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put("input.topic.name", "input-topic");
+        properties.put("output.topic.name", "output-topic");
+        final Producer<String, String> producer = new KafkaProducer<>(properties);
+        System.out.println(producer);
+        final String topic = properties.getProperty("output.topic.name");
+        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, "my-key", "my-value");
+        final Future<RecordMetadata> future = producer.send(record);
+        final RecordMetadata metadata = future.get();
+        System.out.println(metadata);
+    }
+}
