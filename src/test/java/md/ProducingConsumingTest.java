19ff174acd57c3eab4d8c3a0cd5e80d85d5e9e0a
Marek Dudek <marek.dudek@gmail.com>
2021-07-14 23:24:27 +0200

    simplified

--- src/test/java/md/ProducingConsumingTest.java
+++ src/test/java/md/ProducingConsumingTest.java
@@ -11,43 +11,46 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import static java.time.Duration.ofSeconds;
+import static java.util.Collections.singletonList;
 import static md.PropertiesHelper.fromFile;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-class ProducingConsumingTest
+final class ProducingConsumingTest
 {
+    private static final String PROPERTIES = "src/main/resources/confluent-platform-6.2.0.properties";
+    private static final String TOPIC = "some-topic";
+
     @Test
     void produce() throws ExecutionException, InterruptedException, IOException
     {
-        final Properties properties = fromFile("src/main/resources/confluent-platform-6.2.0.properties");
-        final ProducerRecord<String, String> record = new ProducerRecord<>("some-topic", "some-key", "some-value");
-        final Producer<String, String> producer = new KafkaProducer<>(properties);
-        final Future<RecordMetadata> future = producer.send(record);
-        final RecordMetadata metadata = future.get();
-        log.info("metadata: {}", metadata);
-        assertThat(metadata.hasOffset()).isTrue();
-        assertThat(metadata.hasTimestamp()).isTrue();
-        producer.close();
+        final Properties ps = fromFile(PROPERTIES);
+        final ProducerRecord<String, String> r = new ProducerRecord<>(TOPIC, "some-key", "some-value");
+        final Producer<String, String> p = new KafkaProducer<>(ps);
+        final Future<RecordMetadata> f = p.send(r);
+        final RecordMetadata md = f.get();
+        log.info("md: {}", md);
+        assertThat(md.hasOffset()).isTrue();
+        assertThat(md.hasTimestamp()).isTrue();
+        p.close();
     }
 
     @Test
     void consume() throws IOException
     {
-        final Properties properties = fromFile("src/main/resources/confluent-platform-6.2.0.properties");
-        final Consumer<String, String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Collections.singletonList("some-topic"));
+        final Properties ps = fromFile(PROPERTIES);
+        final Consumer<String, String> c = new KafkaConsumer<>(ps);
+        c.subscribe(singletonList(TOPIC));
         log.info("Polling ...");
-        final ConsumerRecords<String, String> records = consumer.poll(ofSeconds(10));
-        records.forEach(record -> {
-            log.info("{}:{} ({})", record.key(), record.value(), record.headers());
+        final ConsumerRecords<String, String> rs = c.poll(ofSeconds(10));
+        rs.forEach(r -> {
+            log.info("{}:{} ({})", r.key(), r.value(), r.headers());
         });
-        consumer.close();
+        c.close();
     }
 }
