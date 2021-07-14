b668e34d165be848459a02348a03f706dbc56dc8
Marek Dudek <marek.dudek@gmail.com>
2021-07-14 23:20:22 +0200

    consumed

--- src/test/java/md/ProducingConsumingTest.java
+++ src/test/java/md/ProducingConsumingTest.java
@@ -1,6 +1,9 @@
 package md;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -8,18 +11,20 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import static java.time.Duration.ofSeconds;
 import static md.PropertiesHelper.fromFile;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-class ProducerTest
+class ProducingConsumingTest
 {
     @Test
-    void producing() throws ExecutionException, InterruptedException, IOException
+    void produce() throws ExecutionException, InterruptedException, IOException
     {
         final Properties properties = fromFile("src/main/resources/confluent-platform-6.2.0.properties");
         final ProducerRecord<String, String> record = new ProducerRecord<>("some-topic", "some-key", "some-value");
@@ -29,5 +34,20 @@ class ProducerTest
         log.info("metadata: {}", metadata);
         assertThat(metadata.hasOffset()).isTrue();
         assertThat(metadata.hasTimestamp()).isTrue();
+        producer.close();
+    }
+
+    @Test
+    void consume() throws IOException
+    {
+        final Properties properties = fromFile("src/main/resources/confluent-platform-6.2.0.properties");
+        final Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList("some-topic"));
+        log.info("Polling ...");
+        final ConsumerRecords<String, String> records = consumer.poll(ofSeconds(10));
+        records.forEach(record -> {
+            log.info("{}:{} ({})", record.key(), record.value(), record.headers());
+        });
+        consumer.close();
     }
 }
